d317e11f7fe926170af4d31ca0936f6edaec7266
Cris Stef <cristiane.souza1234@hotmail.com>
2019-07-29 17:07:26 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/crissantana/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.crissantana.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crissantana.cursomc.domain.Categoria;
+import com.crissantana.cursomc.domain.Cliente;
+import com.crissantana.cursomc.repositories.ClienteRepository;
+import com.crissantana.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+}

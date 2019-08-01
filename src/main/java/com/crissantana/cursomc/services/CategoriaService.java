95f2d8ac2a99552562f8430f513889bb7784d12a
Cris Stef <cristiane.souza1234@hotmail.com>
2019-08-01 15:25:09 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/crissantana/cursomc/services/CategoriaService.java
+++ src/main/java/com/crissantana/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.crissantana.cursomc.domain.Categoria;
+import com.crissantana.cursomc.dto.CategoriaDTO;
 import com.crissantana.cursomc.repositories.CategoriaRepository;
 import com.crissantana.cursomc.services.exceptions.DataIntegrityException;
 import com.crissantana.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,4 +56,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	
 }
